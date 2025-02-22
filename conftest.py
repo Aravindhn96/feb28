e2b2953a6f677f25b71dec94ffdfd1e6e2012ea2
aravindh10007 <aravindhn115@gmail.com>
2025-02-22 08:23:28 +0530

    first project

--- conftest.py
+++ conftest.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 @pytest.fixture(scope= "session")
 def driver():
     driver = webdriver.Firefox()
+    driver.maximize_window()
     driver.get("https://practicetestautomation.com/practice-test-login/")
 
     yield driver
     driver.quit()
+
